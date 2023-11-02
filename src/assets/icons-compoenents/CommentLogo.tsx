a4d9168977770b3baeb97cefcdc0d0ae2259becf
Kausar Hasan <kausarhsn92@gmail.com>
2023-11-02 23:04:18 +0600

    icon color change

--- src/assets/icons-compoenents/CommentLogo.tsx
+++ src/assets/icons-compoenents/CommentLogo.tsx
@@ -1,7 +1,7 @@
 
 const CommentLogo = () => {
   return (
-    <svg xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" aria-label="Comment" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Comment</title><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="#000000" stroke-linejoin="round" stroke-width="2px"></path></svg>
+    <svg xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" aria-label="Comment" className="fill-black stroke-black"  height="24" role="img" viewBox="0 0 24 24" width="24"><title>Comment</title><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke-linejoin="round" stroke-width="2px"></path></svg>
   )
 }
 
